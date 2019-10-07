9cac396464fc4c178789da9114ee2f6a435a53ab
Your Name <cesar.simionato@sou.unifeob.edu.br>
2019-10-06 23:24:32 -0300

    fim :)

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,35 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io')
+const http = require('http')
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app)
+const io = socketio(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-y8xkp.mongodb.net/dbomnistack?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
+
+
+const connectedUsers = {}; 
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+})
+
 
 app.use(cors());
 app.use(express.json());
@@ -19,4 +40,4 @@ app.use(routes);
 
 
 
-app.listen(3333);
+server.listen(3333);
